922530a9322b9e6b5f83f630015f303cd4fe7421
coco_dev <51035468+nicolasabot@users.noreply.github.com>
2023-02-04 20:55:38 +0100

    Update interactionCreate.js

--- Events/interactionCreate.js
+++ Events/interactionCreate.js
@@ -1,7 +1,17 @@
-const { DiscordSnowflake } = require("@sapphire/snowflake")
 const Discord = require("discord.js")
 
 module.exports = async (bot, interaction) => {
+
+  if(interaction.type === Discord.InteractionType.ApplicationCommandAutocomplete) {
+
+    let entry = interaction.options.getFocused()
+
+    if(interaction.commandName === "help") {
+
+    let choices = bot.commands.filter(cmd => cmd.name.includes(entry))
+    await interaction.respond(entry === "" ? bot.commands.map(cmd => ({name: cmd.name, value: cmd.name})) : choices.map(choice => ({name: choice.name, value: choice.name})))
+    }
+  }
 
   if(interaction.type === Discord.InteractionType.ApplicationCommand) {
 
