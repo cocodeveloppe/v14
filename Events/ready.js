2c00d26b0b3c08f711849790ab90dff5bd0a58ba
coco_dev <51035468+cocodeveloppe@users.noreply.github.com>
2023-02-04 21:34:42 +0100

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -1,7 +1,13 @@
 const Discord = require("discord.js")
+const loadDatabase = require("../Loader/loadDatabase")
 const loadSlashCommands = require("../Loader/loadSlashCommands")
 
 module.exports = async bot => {
+
+  bot.db = await loadDatabase ()
+  bot.db.connect(function() {
+    console.log("Base de données connectée !")
+  })
 
   await loadSlashCommands(bot)
 
