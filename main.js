f4faf6bfd529b52c388be2ddd8b8f4211708f9c1
coco_dev <51035468+cocodeveloppe@users.noreply.github.com>
2023-02-04 21:34:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,15 +3,11 @@ const intents = new Discord.IntentsBitField(3276799)
 const bot = new Discord.Client({intents})
 const loadCommands = require("./Loader/loadCommands")
 const loadEvents = require("./Loader/loadEvents")
-const loadDatabase = require("./Loader/loadDatabase")
 const config = require("./config")
 
 bot.commands = new Discord.Collection()
 bot.color = "#ffff";
-bot.db = loadDatabase()
 
 bot.login(config.token)
 loadCommands(bot)
 loadEvents(bot)
-
-console.log("Base de données connectée  avec succès !")
